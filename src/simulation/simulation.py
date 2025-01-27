765fe1c355ecf3dd2d1ce22da44a971b9d068004
fabrizio.borrello <fabrizio.borrello@pierer-innovation.com>
2025-01-27 17:28:52 +0100

    Add FishTankSimulator and thread-safe simulation data handling
    Introduced FishTankSimulator initialization and integrated it with the simulation flow. Added thread-safe access for simulation data using a Lock to ensure data consistency. Adjusted simulation logic to accommodate the new simulator and improved configuration management.

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -1,20 +1,36 @@
 import asyncio
 import json
 import os
-
+from threading import Lock
+from src.simulation.fish_tank_simulation import FishTankSimulator
 from src.simulation.seasonal_weather_simulation import SeasonalWeatherSimulator
 
 class ArtificialEcosystemSimulator:
     def __init__(self, configuration_files_path: str, country: str):
         self.configuration_files_path = None
-        self.simulation_data = None
+        self.simulation_config = None
+        self._simulation_data = dict()
+        self._data_lock = Lock()
         self.configuration_files_lst = self._get_configuration_files(configuration_files_path)
         self._get_simulation_data()
         self.country = country
         self.seasonal_weather_simulator = self._init_seasonal_weather_simulator()
+        self.seasonal_weather_simulator.simulation_data = self.simulation_data
+        self.fish_tank_simulator = self._init_fish_tank()
+        self.fish_tank_simulator.simulation_data = self.simulation_data
         self.sim_tasks = dict()
 
+    @property
+    def simulation_data(self):
+        with self._data_lock:
+            return self._simulation_data
 
+    @simulation_data.setter
+    def simulation_data(self, new_data: dict):
+        with self._data_lock:
+            self._simulation_data = new_data
+
+    
     def _get_configuration_files(self, path: str):
         base_dir = os.path.dirname(os.path.abspath(__file__))
         config_path = os.path.join(base_dir, path)
@@ -46,8 +62,7 @@ class ArtificialEcosystemSimulator:
         for config_file in self.configuration_files_lst:
             if "simulation" in config_file:
                 with open(os.path.join(self.configuration_files_path, config_file), "r") as f:
-                    simulation_data = json.load(f)
-                self.simulation_data = simulation_data
+                    self.simulation_config = json.load(f)
 
     def _init_seasonal_weather_simulator(self):
         country = self.country.lower()
@@ -57,10 +72,18 @@ class ArtificialEcosystemSimulator:
                     seasonal_weather_config = json.load(f)
                 return SeasonalWeatherSimulator(**seasonal_weather_config)
 
+    def _init_fish_tank(self):
+        for config_file in self.configuration_files_lst:
+            if 'fish_tank' in config_file:
+                with open(os.path.join(self.configuration_files_path, config_file), "r") as f:
+                    fish_tank_config = json.load(f)
+                return FishTankSimulator(**fish_tank_config)
+
     async def simulate(self):
-        seasonal_weather_task = asyncio.create_task(self.seasonal_weather_simulator.simulate(self.simulation_data))
+        seasonal_weather_task = asyncio.create_task(self.seasonal_weather_simulator.simulate(self.simulation_config))
         self.sim_tasks['seasonal_weather'] = seasonal_weather_task
-        weather_sim_data = self.seasonal_weather_simulator.simulation_data
+        fish_tank_task = asyncio.create_task(self.fish_tank_simulator.simulate(self.simulation_config))
+        self.sim_tasks['fish_tank'] = fish_tank_task
 
         while True:
             try:
